e9b96ebaa1059221a3beafa1e09ef6c48f0a59aa
Léo Serrão Barbosa <leo_serrao@hotmail.com>
2020-06-17 15:03:47 -0300

    terceiro commit

--- src/exemplo.js
+++ src/exemplo.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let mensagem;
+/* let mensagem;
 
 mensagem = 3;
 
@@ -10,20 +10,24 @@ mensagem = "oi";
 
 alert(mensagem);
 
+//declarando várias variáveis no mesmo comando
 let usuario = "Léo",
     idade = 38,
     profissao = "Analista de Sistemas";
 
+//atribuindo o valor de uma variável a outra variável
 mensagem = profissao;
 
 alert(mensagem);
 
+//declarando constantes
 const ANIVERSARIO = '05.03.1981';
 
 const VERMELHO = 'F00';
 
 const LARANJA = "FF7F00";
 
+//atribuindo o valor de uma constante a uma variável
 let cor = VERMELHO;
 
 alert(cor)
@@ -32,7 +36,51 @@ cor = LARANJA;
 
 alert(cor)
 
+//tipagem de dados dinâmica
 let texto = "Olá";
 
-texto = 12.3;
+texto = 12.3; 
 
+//Tipo de dados Infinito
+let numero = 1/0;
+alert(numero)
+
+let numero2 = Infinity;
+alert(numero2)
+
+//Tipo de dados inválido
+let numero3 = "oi" / 2;
+alert(numero3)
+
+
+//Tipo de dados númerico
+let num1 = 123;
+
+//tipo de dados real
+num1 = 12.5;
+*/
+
+//tipo de dados lógico
+let maior = true;
+maior = false;
+maior = 4 > 2;
+maior = 4 < 1;
+
+//Valor nulo
+let nulo = null;
+
+//Valor indefinido
+let valor;
+alert(valor)
+
+valor = undefined;
+alert(valor)
+
+
+let texto2 = "oi";
+
+//Mostrando o conteúdo de uma variável em uma mensagem
+alert(`O conteúdo de texto2 é , ${texto2}`);
+
+//Fazendo uma operação aritmética em uma mensagem
+alert(`A soma de 2 + 3 é ${2+3}`)
