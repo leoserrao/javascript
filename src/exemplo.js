d7abcdacca17b32b5d4799408522cd3e191b7ae7
Léo Serrão Barbosa <leo_serrao@hotmail.com>
2020-06-17 12:27:02 -0300

    primeiro commit

---
+++ src/exemplo.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let mensagem;
+
+mensagem = 3;
+
+alert(mensagem);
+
+mensagem = "oi";
+
+alert(mensagem);
+
+let usuario = "Léo",
+    idade = 38,
+    profissao = "Analista de Sistemas";
+
+mensagem = profissao;
+
+alert(mensagem);
+
+const ANIVERSARIO = '05.03.1981';
+
+const VERMELHO = 'F00';
+
+const LARANJA = "FF7F00";
+
+let cor = VERMELHO;
+
+alert(cor)
+
+cor = LARANJA;
+
+alert(cor)
+
+let texto = "Olá";
+
+texto = 12.3;
+
